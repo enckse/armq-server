83780f369de5a3d1a890bc3feed10e69c8396ad5
Sean Enck <enckse@gmail.com>
2018-01-02 12:42:40 -0500

    actual tag detection

--- armq_server.py
+++ armq_server.py
@@ -118,9 +118,12 @@ def interrogate(q):
                     if len(parts) >= 1:
                         tag = parts[1]
                         if tag not in tracked:
-                            log.info("new tag detected ({})".format(tag))
-                            subprocess.call(["/usr/bin/didumumble-signal"])
-                            tracked.append(tag)
+                            t = "".join([x for x in tag \
+                                         if x >= 'a' and x <= 'z'])
+                            if len(t) == 4 and t == tag:
+                                log.info("new tag detected ({})".format(tag))
+                                subprocess.call(["/usr/bin/didumumble-signal"])
+                                tracked.append(tag)
         except Exception as e:
             log.warn("interrogation error")
             log.warn(e)
