a24c61e227b2f33f8cb20601a66d092b41859e64
Sean Enck <enckse@voidedtech.com>
2019-03-29 08:28:54 -0400

    merged these definitions

--- internal/receiver.go
+++ internal/receiver.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -20,6 +21,8 @@ var (
 	readLock = &sync.Mutex{}
 	objcache = []*object{}
 	gc       = []string{}
+	lock     = &sync.Mutex{}
+	cache    = make(map[string]struct{})
 )
 
 const (
@@ -246,5 +249,86 @@ func RunReceiver() {
 	}
 	for {
 		time.Sleep(1)
+	}
+}
+
+func runCollector(conf *fileConfig) {
+	files := collect()
+	lock.Lock()
+	defer lock.Unlock()
+	for _, f := range files {
+		p := filepath.Join(conf.directory, f)
+		logger.WriteDebug("collecting", p)
+		_, e := opsys.RunBashCommand(fmt.Sprintf("rm -f %s", p))
+		if e != nil {
+			logger.WriteWarn("file error on gc", p)
+			logger.WriteError("unable to remove garbage", e)
+		}
+		// we are good to no longer know about this
+		if _, ok := cache[f]; ok {
+			delete(cache, f)
+		}
+	}
+}
+
+type fileConfig struct {
+	directory string
+	after     time.Duration
+	gc        int
+	sleep     time.Duration
+}
+
+func scan(conf *fileConfig) {
+	files, e := ioutil.ReadDir(conf.directory)
+	if e != nil {
+		logger.WriteError("unable to scan files", e)
+		return
+	}
+	lock.Lock()
+	defer lock.Unlock()
+	requiredTime := time.Now().Add(conf.after * time.Second)
+	for _, f := range files {
+		n := f.Name()
+		// if we already read this file we certainly should not read it again
+		if _, ok := cache[n]; ok {
+			continue
+		}
+		if f.ModTime().After(requiredTime) {
+			continue
+		}
+		logger.WriteDebug("reading", n)
+		cache[n] = struct{}{}
+		p := filepath.Join(conf.directory, n)
+		d, e := ioutil.ReadFile(p)
+		if e != nil {
+			logger.WriteWarn("file read error", p)
+			logger.WriteError("unable to read file", e)
+			continue
+		}
+		queue(n, d, true)
+	}
+}
+
+func fileReceive(config *Configuration) {
+	conf := &fileConfig{}
+	conf.directory = config.Files.Directory
+	conf.gc = config.Files.Gc
+	conf.sleep = time.Duration(config.Files.Sleep)
+	conf.after = time.Duration(config.Files.After)
+	logger.WriteInfo("file mode enabled")
+	err := os.Mkdir(conf.directory, 0777)
+	if err != nil {
+		logger.WriteError("unable to create directory (not aborting)", err)
+	}
+	lastCollected := 0
+	for {
+		if lastCollected > conf.gc {
+			logger.WriteDebug("collecting garbage")
+			runCollector(conf)
+			lastCollected = 0
+		}
+		scan(conf)
+		time.Sleep(conf.sleep * time.Millisecond)
+		lastCollected += 1
 	}
 }
